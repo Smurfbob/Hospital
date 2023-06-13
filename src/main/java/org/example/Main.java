d0f3364c7c4dab33c6082050a03368d5b1fd490d
hannEzz92 <hannah.ezzoubeidi@outlook.de>
2023-06-13 11:38:51 +0200

    database working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,15 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main (String[] args) {
-
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5432/hospital", "postgres", "postgres")) {
+                "jdbc:postgresql://127.0.0.1:5432/hospital", "postgres", System.getenv("password"))) {
 
             if (conn != null) {
                 System.out.println("Connected to the database!");
+                printOrtTable(conn);
             } else {
                 System.out.println("Failed to make connection!");
             }
@@ -19,6 +17,29 @@ public class Main {
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void printOrtTable(Connection conn) {
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM Ort")) {
+
+            while (rs.next()) {
+                // Retrieve data from each row
+                int plz = rs.getInt("plz");
+                String name = rs.getString("name");
+                String location = rs.getString("region");
+
+                // Print the data
+                System.out.println("ID: " + plz);
+                System.out.println("Name: " + name);
+                System.out.println("Location: " + location);
+                System.out.println("--------------------------");
+            }
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
