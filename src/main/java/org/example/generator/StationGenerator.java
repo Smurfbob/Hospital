7c25507a12f63f690fb9d3a15de25c5fa9b2c67a
hannEzz92 <hannah.ezzoubeidi@outlook.de>
2023-06-14 13:15:32 +0200

    wip

--- src/main/java/org/example/generator/StationGenerator.java
+++ src/main/java/org/example/generator/StationGenerator.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StationGenerator {
@@ -15,7 +16,7 @@ public class StationGenerator {
     private static int ID_COUNTER = 0;
 
     public static int r() {
-        return ThreadLocalRandom.current().nextInt(100);
+        return ThreadLocalRandom.current().nextInt(20);
     }
 
     public static List<Station> getAmountOfRandomStationFachrichtung(final Collection<Krankenhaus> hauser, final int stationAmountForHospital) {
@@ -23,11 +24,20 @@ public class StationGenerator {
         final List<Station> stations = new ArrayList<>();
         for(Krankenhaus krankenhaus : hauser) {
             for(int i=0 ; i < stationAmountForHospital ; i++) {
-                final Station station = new Station(++ID_COUNTER, faker.name().fullName(), r(), r(), krankenhaus.getPlz());
+                Station station = new Station(++ID_COUNTER, removeSymbols(faker.name().fullName()), r(), r(), krankenhaus.getPlz());
                 stations.add(station);
             }
         }
         return stations;
+    }
+
+    private static String removeSymbols(String input) {
+        // Regular expression to match symbols
+        // Pattern pattern = Pattern.compile("[^a-zA-Z0-9\\s']");
+        // Replace symbols with an empty string
+        // return pattern.matcher(input).replaceAll("");
+
+        return input.replaceAll("'","");
     }
 
 }
