8573f2454d115fd7362b154cdc065fd9c7995900
Dominik Bosum <dbosum@fuzzy.de>
2023-06-14 12:56:15 +0200

    Fixed Merge Conflict

--- src/test/java/org/example/model/KrankenhausTest.java
+++ src/test/java/org/example/model/KrankenhausTest.java
@@ -4,25 +4,49 @@ import org.example.service.ServiceProvider;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
 
-import static org.junit.Assert.*;
 public class KrankenhausTest {
 
-    private void request(final int amount) {
+
+    private static final int DEFAULT_INTERVAL = 5000;
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy:MM:DD-hh:mm:ss");
+
+    private void request(final int amount, final String name) {
         final LocalDateTime now = LocalDateTime.now();
         for(int i=0 ; i < amount ; i++) {
             ServiceProvider.HOSPITAL_SERVICE.getAll();
         }
         final LocalDateTime endTime = LocalDateTime.now();
 
-        // final String response = String.format("%s --- %s Time: %d", now, endTime, );
+        final String response = String.format("%s %s %s --- %s Time: ", name ,KrankenhausTest.class.getSimpleName(),
+                now.format(FORMATTER),
+                endTime.format(FORMATTER) );
+        System.out.println(response);
     }
 
 
+
+
+    @Test
+    public void test0() {
+        request(DEFAULT_INTERVAL * 1, "Test 1");
+    }
+
     @Test
     public void test1() {
+        request(DEFAULT_INTERVAL * 5, "Test 5");
+    }
 
+    @Test
+    public void test2() {
+        request( DEFAULT_INTERVAL * 10, "Test 10");
+    }
+
+    @Test
+    public void test3() {
+        request( DEFAULT_INTERVAL * 20, "Test 20");
     }
 
   
---
+++ src/test/java/org/example/model/Utils.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+import java.time.Instant;
+
+public class Utils {
+
+
+    public static String getDateString(final Instant a, final Instant b) {
+
+    }
+}
