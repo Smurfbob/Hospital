d61ad2d2a5015233e1ade1929514bc27995e385a
Dominik Bosum <dbosum@fuzzy.de>
2023-06-15 07:53:46 +0200

    Created linear tests for Hospital

--- src/test/java/org/example/model/KrankenhausTest.java
+++ src/test/java/org/example/model/KrankenhausTest.java
@@ -1,23 +1,54 @@
 package org.example.model;
 
 import org.example.service.ServiceProvider;
+import org.example.template.DataAccess;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 public class KrankenhausTest {
 
-    @Test
-    public void test0() {
 
+    private static int DEFAULT_REQUESTS = 5000;
+    private static final int DEFAULT_THREAD_AMOUNT = 10000;
+    private static final DataAccess<Krankenhaus> HOSPITAL_SERVICE = ServiceProvider.HOSPITAL_SERVICE;
 
-        final List<Krankenhaus> krankenhaus = ServiceProvider.HOSPITAL_SERVICE.getAll();
-        krankenhaus.forEach(System.out::println);
-
-
+    private static void runWithAmountOfThreads(final int threadAmount, Runnable runnable) {
+        for(int i=0 ; i < threadAmount ; i++) {
+            new Thread(runnable).start();
+        }
     }
+
+    private static void doAmountOfHospitalRequests(final int requestAmount) {
+        for(int i=0 ; i < requestAmount ; i++) {
+            HOSPITAL_SERVICE.getAll();
+        }
+    }
+
+    @Test
+    public void test1() {
+        doAmountOfHospitalRequests(1 * DEFAULT_REQUESTS);
+    }
+
+
+    @Test
+    public void test5() {
+        doAmountOfHospitalRequests(5 * DEFAULT_REQUESTS);
+    }
+
+
+    @Test
+    public void test10() {
+        doAmountOfHospitalRequests(10 * DEFAULT_REQUESTS);
+    }
+
+
+    @Test
+    public void test20() {
+        doAmountOfHospitalRequests(20 * DEFAULT_REQUESTS);
+    }
+
+
 
 
 }
